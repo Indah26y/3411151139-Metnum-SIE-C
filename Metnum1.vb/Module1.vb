6cd291caabd18252e79d36af208db3cca372658b
INDAH PUTRIANI FAJAR SIDIK <32114406+Indah26y@users.noreply.github.com>
2017-09-21 21:36:32 +0700

    Add files via upload

---
+++ Metnum1.vb/Module1.vb
@@ -0,0 +1,61 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim A As Integer
+        Dim B As Integer
+        Dim hasil As Double
+
+        Console.WriteLine("")
+        Console.WriteLine("=================================")
+        Console.WriteLine(" METODE NUMERIK ")
+        Console.WriteLine("Nama : Indah Putriani Fajar Sidik")
+        Console.WriteLine("Nim  : 3411151139")
+        Console.WriteLine("Kelas: SIE C")
+        Console.WriteLine("=================================")
+
+        For i = 0 To 10
+            Console.WriteLine("")
+            Console.WriteLine("Tampilkan Menu")
+            Console.WriteLine("")
+            Console.WriteLine("1.A + B")
+            Console.WriteLine("2.A - B")
+            Console.WriteLine("3.A / B")
+            Console.WriteLine("4.A ^ B")
+            Console.WriteLine("5.Deret F(X)=2A^2-4A+1")
+            Console.WriteLine("7. KELUAR")
+            Console.WriteLine("")
+            hasil = Console.ReadLine()
+            Console.WriteLine("")
+
+            Console.Write("Masukan nilai A:")
+            A = Console.ReadLine()
+            Console.Write("Masukan nilai B:")
+            B = Console.ReadLine()
+
+            Select Case hasil
+                Case 1
+                    hasil = A + B
+                    Console.WriteLine("Hasil :" & hasil)
+                Case 2
+                    hasil = A - B
+                    Console.WriteLine("Hasil :" & hasil)
+                Case 3
+                    hasil = A / B
+                    Console.WriteLine("Hasil :" & hasil)
+                Case 4
+                    hasil = A ^ B
+                    Console.WriteLine("Hasil :" & hasil)
+                Case 5
+                    Console.Write("Masukan Nilai A:")
+                    A = Console.ReadLine()
+                    Console.Write("Masukan Nilai B:")
+                    B = Console.ReadLine()
+                Case 6
+                    End
+                    Console.WriteLine("")
+            End Select
+
+        Next
+    End Sub
+
+End Module
---
+++ Metnum1.vb/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Metnum1.vb")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Toshiba")> 
+<Assembly: AssemblyProduct("Metnum1.vb")> 
+<Assembly: AssemblyCopyright("Copyright © Toshiba 2017")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("d94547d4-dcbe-428f-a3e5-16af72c33ca4")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
